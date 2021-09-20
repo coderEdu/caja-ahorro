19f02bd2f6aa23823036a804457068dd76f90399
EduCoder <educoder78@gmail.com>
2021-09-19 22:59:39 -0300

    implem log-transact message complete

--- CajaDeAhorro/C_AHORRO_NEW_DS.Designer.cs
+++ CajaDeAhorro/C_AHORRO_NEW_DS.Designer.cs
@@ -3734,7 +3734,7 @@ SELECT id, fecha, tipo, monto, id_log FROM movi WHERE (id = @id)";
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[3];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT id, mensaje FROM dbo.mensaje";
@@ -3745,6 +3745,13 @@ SELECT id, fecha, tipo, monto, id_log FROM movi WHERE (id = @id)";
                 "           movi ON mensaje.id = movi.id\r\nWHERE        (mensaje.id = @movi_id)";
             this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@movi_id", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "id", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[2].Connection = this.Connection;
+            this._commandCollection[2].CommandText = "INSERT INTO mensaje\r\n                         (id, mensaje)\r\nVALUES        (@movi" +
+                "_id,@mensaje); \r\nSELECT id, mensaje FROM mensaje WHERE (id = @movi_id)";
+            this._commandCollection[2].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@movi_id", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "id", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@mensaje", global::System.Data.SqlDbType.NVarChar, 150, global::System.Data.ParameterDirection.Input, 0, 0, "mensaje", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -3927,6 +3934,36 @@ SELECT id, fecha, tipo, monto, id_log FROM movi WHERE (id = @id)";
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, true)]
         public virtual int Update(string mensaje, int Original_id, string Original_mensaje) {
             return this.Update(Original_id, mensaje, Original_id, Original_mensaje);
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Insert, false)]
+        public virtual int MessageInsertQuery(int movi_id, string mensaje) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[2];
+            command.Parameters[0].Value = ((int)(movi_id));
+            if ((mensaje == null)) {
+                command.Parameters[1].Value = global::System.DBNull.Value;
+            }
+            else {
+                command.Parameters[1].Value = ((string)(mensaje));
+            }
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
         }
     }
     
--- CajaDeAhorro/Transaccion.Designer.cs
+++ CajaDeAhorro/Transaccion.Designer.cs
@@ -34,8 +34,11 @@ namespace CajaDeAhorro
             this.txt_trans_monto = new System.Windows.Forms.TextBox();
             this.dtp = new System.Windows.Forms.DateTimePicker();
             this.c_AHORRO_NEW_DS1 = new CajaDeAhorro.C_AHORRO_NEW_DS();
-            this.loginTableAdapter1 = new CajaDeAhorro.C_AHORRO_NEW_DSTableAdapters.loginTableAdapter();
             this.moviTableAdapter2 = new CajaDeAhorro.C_AHORRO_NEW_DSTableAdapters.moviTableAdapter();
+            this.txt_concepto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginTableAdapter1 = new CajaDeAhorro.C_AHORRO_NEW_DSTableAdapters.loginTableAdapter();
+            this.mensajeTableAdapter1 = new CajaDeAhorro.C_AHORRO_NEW_DSTableAdapters.mensajeTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.c_AHORRO_NEW_DS1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,10 +50,10 @@ namespace CajaDeAhorro
             this.btn_trans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_trans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_trans.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btn_trans.Location = new System.Drawing.Point(98, 117);
+            this.btn_trans.Location = new System.Drawing.Point(100, 183);
             this.btn_trans.Name = "btn_trans";
             this.btn_trans.Size = new System.Drawing.Size(86, 29);
-            this.btn_trans.TabIndex = 1;
+            this.btn_trans.TabIndex = 2;
             this.btn_trans.Text = "Efectuar";
             this.btn_trans.UseVisualStyleBackColor = false;
             this.btn_trans.Click += new System.EventHandler(this.btn_trans_Click);
@@ -58,9 +61,9 @@ namespace CajaDeAhorro
             // lbl_trans
             // 
             this.lbl_trans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_trans.Location = new System.Drawing.Point(0, 27);
+            this.lbl_trans.Location = new System.Drawing.Point(0, 34);
             this.lbl_trans.Name = "lbl_trans";
-            this.lbl_trans.Size = new System.Drawing.Size(278, 36);
+            this.lbl_trans.Size = new System.Drawing.Size(278, 29);
             this.lbl_trans.TabIndex = 2;
             this.lbl_trans.Text = "Monto:";
             this.lbl_trans.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,7 +71,7 @@ namespace CajaDeAhorro
             // txt_trans_monto
             // 
             this.txt_trans_monto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_trans_monto.Location = new System.Drawing.Point(71, 66);
+            this.txt_trans_monto.Location = new System.Drawing.Point(77, 66);
             this.txt_trans_monto.MaxLength = 6;
             this.txt_trans_monto.Multiline = true;
             this.txt_trans_monto.Name = "txt_trans_monto";
@@ -89,20 +92,47 @@ namespace CajaDeAhorro
             this.c_AHORRO_NEW_DS1.DataSetName = "C_AHORRO_NEW_DS";
             this.c_AHORRO_NEW_DS1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // moviTableAdapter2
+            // 
+            this.moviTableAdapter2.ClearBeforeFill = true;
+            // 
+            // txt_concepto
+            // 
+            this.txt_concepto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_concepto.Location = new System.Drawing.Point(31, 136);
+            this.txt_concepto.MaxLength = 145;
+            this.txt_concepto.Multiline = true;
+            this.txt_concepto.Name = "txt_concepto";
+            this.txt_concepto.Size = new System.Drawing.Size(221, 28);
+            this.txt_concepto.TabIndex = 1;
+            this.txt_concepto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "En concepto de:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // loginTableAdapter1
             // 
             this.loginTableAdapter1.ClearBeforeFill = true;
             // 
-            // moviTableAdapter2
+            // mensajeTableAdapter1
             // 
-            this.moviTableAdapter2.ClearBeforeFill = true;
+            this.mensajeTableAdapter1.ClearBeforeFill = true;
             // 
             // Transaccion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(282, 176);
+            this.ClientSize = new System.Drawing.Size(282, 224);
+            this.Controls.Add(this.txt_concepto);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dtp);
             this.Controls.Add(this.txt_trans_monto);
             this.Controls.Add(this.lbl_trans);
@@ -129,5 +159,8 @@ namespace CajaDeAhorro
         private C_AHORRO_NEW_DS c_AHORRO_NEW_DS1;
         private C_AHORRO_NEW_DSTableAdapters.loginTableAdapter loginTableAdapter1;
         private C_AHORRO_NEW_DSTableAdapters.moviTableAdapter moviTableAdapter2;
+        private System.Windows.Forms.TextBox txt_concepto;
+        private System.Windows.Forms.Label label1;
+        private C_AHORRO_NEW_DSTableAdapters.mensajeTableAdapter mensajeTableAdapter1;
     }
 }

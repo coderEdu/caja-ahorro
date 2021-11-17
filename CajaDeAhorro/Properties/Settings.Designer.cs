06c62eff280d16938cad9f5bc9badb4c6de31f75
coderEdu <gtpanteraxtr@gmail.com>
2021-11-17 00:57:21 -0300

    changes

--- CajaDeAhorro/Properties/Settings.Designer.cs
+++ CajaDeAhorro/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CajaDeAhorro.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace CajaDeAhorro.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-33FBPTR\\EDUCODER_SQL;Initial Catalog=CAJA_AHORRO;Integrated S" +
-            "ecurity=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-RIMH7AG\\EDUCODERSQL;Initial Catalog=CAJA_AHORRO;Integrated Se" +
+            "curity=True")]
         public string CAJA_AHORROConnectionString {
             get {
                 return ((string)(this["CAJA_AHORROConnectionString"]));

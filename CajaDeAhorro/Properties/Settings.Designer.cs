350aa546289b1ba6a4cee1f79da916f76f23be25
educoder <educoder78@gmail.com>
2021-12-16 18:37:18 -0300

    some merger fixes

--- CajaDeAhorro/Properties/Settings.Designer.cs
+++ CajaDeAhorro/Properties/Settings.Designer.cs
@@ -12,15 +12,8 @@ namespace CajaDeAhorro.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-<<<<<<< HEAD
-<<<<<<< HEAD
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
-=======
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
->>>>>>> origin/dev
-=======
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
->>>>>>> origin/dev
+
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -34,18 +27,8 @@ namespace CajaDeAhorro.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-<<<<<<< HEAD
-<<<<<<< HEAD
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-RIMH7AG\\EDUCODERSQL;Initial Catalog=CAJA_AHORRO;Integrated Se" +
-            "curity=True")]
-=======
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-6I0A1DA\\SQLEDUCODER78;Initial Catalog=CAJA_AHORRO;Integrated " +
-            "Security=True")]
->>>>>>> origin/dev
-=======
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-RIMH7AG\\EDUCODERSQL;Initial Catalog=CAJA_AHORRO;Integrated Se" +
-            "curity=True")]
->>>>>>> origin/dev
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-CBMQEMTK\\EDUCODERSQL;Initial Catalog=CAJA_AHORRO;Integrated Security=True")]
+ 
         public string CAJA_AHORROConnectionString {
             get {
                 return ((string)(this["CAJA_AHORROConnectionString"]));
